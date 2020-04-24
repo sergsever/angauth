8fcb077325f1168cba77d5a9b826aed2a3638d99
Sergey Lvov <sergey.lvov70@gmail.com>
2020-04-24 16:11:10 +0300

    the form only

---
+++ angauth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace angauth.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
